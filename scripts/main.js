2601a9b7cf57b90a63c3588e5d28fed5047b0ecb
unholyhealer-collab <unholyhealer1@gmail.com>
2021-11-05 20:08:33 +0300

    sm changes2

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,22 @@ window.addEventListener('scroll', ()=>{
     var navbar = document.querySelector('nav');
     navbar.classList.toggle('sticky', window.scrollY > 0);
 });
+
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        dots: true,
+        arrows: false
+    });
+});
+
+const whatsapp = document.querySelector('#whatsapp');
+const instagram = document.querySelector('#instagram');
+
+whatsapp.addEventListener('click', ()=>{
+    window.open();
+});
+
+instagram.addEventListener('click', ()=>{
+    window.open('https://www.instagram.com/timur_mironyuk/');
+});
